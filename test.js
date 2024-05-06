dbfe1d37f221d760dc895669ab22099b07205399
Thamilmaran Mohanarasa <thamilmaranm.20@itfac.mrt.ac.lk>
2024-05-06 18:51:52 +0530

    Timeouts

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
 const today = new Date();
 console.log(today.toDateString());
 
-console.log(today.toLocaleDateString())
+console.log(today.toLocaleDateString());
+
+console.log(Date.now());
+
+console.log('print immediately');
+
+setTimeout (() => console. log('prints 1 second later'), 1000);
+
+console.log('print immediately?');
+

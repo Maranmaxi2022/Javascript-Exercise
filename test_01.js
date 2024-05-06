5460b9c0a9502d23fafa4522ceec1433df984627
Thamilmaran Mohanarasa <thamilmaranm.20@itfac.mrt.ac.lk>
2024-05-07 01:48:09 +0530

    Create Dates and Times testing file number 02 and change the file name of test.js file to test_01.js

--- test_01.js
+++ test_01.js
@@ -8,6 +8,5 @@ console.log(Date.now());
 console.log('print immediately');
 
 setTimeout (() => console. log('prints 1 second later'), 1000);
-
 console.log('print immediately?');
 
---
+++ test_02.js
@@ -0,0 +1,10 @@
+// Current date and time
+const now = new Date();
+console.log("Current date and time:", now);
+
+// Specific date: December 25, 2023
+const specificDate = new Date(2023, 11, 25); // Note: month is 0-indexed, so 11 is December
+console.log("Specific date:", specificDate);
+
+const specificDate_02 = new Date(2024,2,21);
+console.log("Specific date:",specificDate_02);
